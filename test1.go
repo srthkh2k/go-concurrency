4db43bd832469fbc49ebafd8f7541b6b6812d34d
Sarthak Kushwaha <kushwahasarthak26@gmail.com>
2021-08-05 21:29:22 +0530

    deadlock demo

---
+++ test1.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+    "fmt"
+    "sync"
+    "time"
+)
+
+type value struct {
+    mu sync.Mutex
+    value int
+}
+
+var wg sync.WaitGroup
+
+printSum := func(v1, v2 *value){
+    defer wg.Done()
+
+    v1.mu.Lock()
+    defer v1.mu.Unlock()
+
+    time.Sleep(2*time.Second)
+    
+    v2.mu.Lock()
+    defer v2.mu.Unlock()
+
+    fmt.Println("sum=%v\n", v1.value+v2.value)
+}
+
+func main() {
+
+    var a, b, value
+    wg.Add(2)
+
+    go printSum(&a, &b)
+    go printSum(&b, &a)
+
+    wg.Wait()
+}
